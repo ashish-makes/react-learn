7c507f1713e2b290dcf17f2d73bb0b10bf33efbd
ashish-makes <ashish.makes@gmail.com>
2024-01-19 17:04:25 -0500

    useState & more functions

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function TextArea() {
-    const [text, setText] = useState("Enter your text here...")
+    const [text, setText] = useState("")
 
     const onChangeHandler = (e) => {
         setText(e.target.value);
@@ -11,11 +11,32 @@ export default function TextArea() {
         setText(text.toUpperCase())
     }
 
+    const lowercaseHandler = () => {
+        setText(text.toLowerCase())
+    }
+    const inversecaseHandler = () => {
+        let invertedText = "";
+        for (let i = 0; i < text.length; i++) {
+            const char = text[i];
+            if (char === char.toLowerCase()) {
+                invertedText += char.toUpperCase();
+            } else {
+                invertedText += char.toLowerCase();
+            }
+        }
+        setText(invertedText);
+    };
+
     return (
         <>
             <label htmlFor='userInput'>Enter text below to change</label><br></br>
-            <textarea id='userInput' value={text} onChange={onChangeHandler}></textarea><br/>
+            <textarea id='userInput' value={text} onChange={onChangeHandler}></textarea><br />
             <button onClick={uppercaseHandler}>Convert to uppercase</button>
+            <button onClick={lowercaseHandler}>Convert to lowercase</button>
+            <button onClick={inversecaseHandler}>Convert to inversecase</button>
+            <br />
+            <p>summary:</p>
+            <p>Words: {text.split(" ").length}  Characters: {text.length}</p>
         </>
     )
 }
